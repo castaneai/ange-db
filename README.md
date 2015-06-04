de122002d105ce5332294e76f28f369d7c921e97
castaneai <castaneai@castaneai.net>
2015-06-04 14:57:31 +0900

    Add README & fix mkdir

---
+++ README.md
@@ -0,0 +1,26 @@
+ange-db
+=========
+Card Viewer for [アンジュ・ヴィエルジュ ～第２風紀委員 ガールズバトル～](http://ange.sega-net.com/)
+
+![screenshot01](https://raw.githubusercontent.com/castaneai/ange-db/master/_readme/screenshot01.png)
+![screenshot02](https://raw.githubusercontent.com/castaneai/ange-db/master/_readme/screenshot02.png)
+
+Usage
+-------
+
+```sh
+$ pip install -r requirements.txt
+$ python app.py
+```
+
+How to create card data from html
+---------------------------------------
+scraping `_crawled/*.html`
+
+```sh
+$ python db_importer.py
+```
+
+Copyright
+-----------
+&copy; [Ange Project](https://ange-vierge.com/)
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
+import os
 import os.path
 from bottle import route, request, template, run, static_file
 import ange.db
 
 
-def static_cache(filepath, create_func):
+def static_cache(filepath, create_data_func):
     if not os.path.exists(filepath):
-        data = create_func()
         with open(filepath, 'wb') as f:
-            f.write(data)
+            f.write(create_data_func())
     return static_file(filepath, root='./')
 
 
@@ -34,5 +34,8 @@ def icon(card_id):
 def static(filepath):
     return static_file(filepath, root='./public')
 
-
+# create image cache dirs
+dirs = ['/public/images/icon', '/public/images/character']
+for d in dirs:
+    os.makedirs(os.path.abspath(os.path.dirname(__file__)) + d, exist_ok=True)
 run(host='127.0.0.1', port=7777, debug=False)
